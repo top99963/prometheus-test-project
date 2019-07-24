e617745f20de2fa34f13223a51347aebd87bb3b1
Top <nonpawit.s@plaping.com>
2019-07-24 09:40:30 +0700

    fix dashborad

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express()
 const port = 3001
 const metricsInterval = Prometheus.collectDefaultMetrics()
 
-
 var response_time_arr = [0.1, 0.3, 1, 1.5, 3, 5]
-var path_arr = ['get_user', 'set_user']
-var response_code_arr = [200, 200, 200, 200, 500]
 
 const histogram = new Prometheus.Histogram({
     name: 'http_request_duration_ms',
@@ -18,14 +15,6 @@ const histogram = new Prometheus.Histogram({
     labelNames: ['method', 'route', 'code', 'success'],
     buckets: response_time_arr
 });
-
-app.get('/random', (req, res, next) => {
-    var responseTime = response_time_arr[Math.floor(Math.random() * response_time_arr.length)]
-    var path = path_arr[Math.floor(Math.random() * path_arr.length)]
-    var code = response_code_arr[Math.floor(Math.random() * response_code_arr.length)]
-    res.status(code).send({ message: responseTime, path: path })
-    histogram.labels('get', path, code).observe(responseTime)
-})
 
 app.get('/register/:success/:code', (req, res, next) => {
     var responseTime = response_time_arr[Math.floor(Math.random() * response_time_arr.length)]
@@ -40,18 +29,6 @@ app.get('/register/:success/:code', (req, res, next) => {
     next()
 })
 
-app.get('/login/:success/:code', (req, res, next) => {
-    var responseTime = response_time_arr[Math.floor(Math.random() * response_time_arr.length)]
-    var result = {
-        route: 'login',
-        message: req.params.success,
-        code: req.params.code,
-        responseTime: responseTime
-    }
-    res.status(req.params.code).send(result)
-    res.data = result
-    next()
-})
 
 app.get('/buy/:success/:code', (req, res, next) => {
     var responseTime = response_time_arr[Math.floor(Math.random() * response_time_arr.length)]
@@ -66,12 +43,28 @@ app.get('/buy/:success/:code', (req, res, next) => {
     next()
 })
 
+app.get('/sell/:success/:code', (req, res, next) => {
+    var responseTime = response_time_arr[Math.floor(Math.random() * response_time_arr.length)]
+    var result = {
+        route: 'sell',
+        message: req.params.success,
+        code: req.params.code,
+        responseTime: responseTime
+    }
+    res.status(req.params.code).send(result)
+    res.data = result
+    next()
+})
+
+
+app.get('/favicon.ico', (req, res) => res.status(204))
+
 app.get('/metrics', (req, res) => {
     res.set('Content-Type', Prometheus.register.contentType)
     res.end(Prometheus.register.metrics())
 })
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var data = res.data
     histogram.labels(req.method, data.route, data.code, data.message).observe(res.data.responseTime)
 })
